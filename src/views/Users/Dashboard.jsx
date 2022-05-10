a8af1c999fcd64f39565df1efa77b398c1f7b6b0
Philippe Ngom <philngom@gmail.com>
2022-05-09 18:15:23 -0700

    started implementing dashboard. need to write db call functions

--- src/views/Users/Dashboard.jsx
+++ src/views/Users/Dashboard.jsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from 'react';
+import { useUser } from '../../context/UserContext';
+
 export default function dashboard() {
+
+  useEffect(() => {
+    // const fetchEntries =
+  }, []);
+
+  const [entry, setEntry] = useState('');
+  const [entries, setEntries] = useState([]);
+
   return (
-    <div>dashboard</div>
+    <h1>Welcome to the dashboard</h1>
+    <label>
+      Add an entry:
+      <input
+      value={entry}
+      />
+    </label>
+    {
+      entries.map((entry) => <p>{entry.entry}</p>)
+    }
   )
 }
